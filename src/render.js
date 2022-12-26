8d38fe7a7d5377d64b1ab4691efbdbf930e00f3a
TURPI <turpicrypto@gmail.com>
2022-12-26 19:56:52 +0200

    Fix incoming data in render.js
    *change import to props

--- src/render.js
+++ src/render.js
@@ -2,16 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
-import state from './Redux/state';
 import { newPost, newMessage } from "./Redux/state";
 
 
-export let rerenderEntireTree = () => {
+export let rerenderEntireTree = (props) => {
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
     <React.StrictMode>
       <BrowserRouter>
-        <App state={state} newPost={newPost} newMessage={newMessage} />
+        <App state={props.state} newPost={newPost} newMessage={newMessage} />
       </BrowserRouter>
     </React.StrictMode>
   );
