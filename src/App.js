daae136afd346fbc20ce9ae1362909d1a8dcfe4f
TURP1 <turpicrypto@gmail.com>
2023-03-01 14:07:07 +0200

    Add basename to aviod reloading mistake 1.0

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ const AppContainer = connect(mapDispatchToProps, { initializeApp })(App);
 
 const NetworkApp = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename ="/">
       <Provider store={store}>
         <AppContainer />
       </Provider>
