0a0792415c8cc5f5de22ee9297b06352084cbf5b
TURP1 <turpicrypto@gmail.com>
2023-03-06 23:25:07 +0200

    Add Profile_Details component

--- src/Components/Profile/Profile__Info/Profile__Info.jsx
+++ src/Components/Profile/Profile__Info/Profile__Info.jsx
@@ -1,33 +1,17 @@
 import React from 'react'
 import obj from './Profile__Info.module.css'
 
-import Status from './Profile__Status_WithHOOK';
+
 import ProfileAva from './Profile_Ava';
+import ProfileDetails from './Profile_Details';
 
 
 const ProfileInfo = ({ currentProfileInfo, changePicture, ...props }) => {
 
-
-
   return (
     <div className={obj.profile__info_container}>
       <ProfileAva currentProfileInfo={currentProfileInfo} changePicture={changePicture} isOwner={props.isOwner}/>
-      <div className={obj.profile__info_details_container}>
-        <div className={obj.profile__info_details_container_row}>
-          <div className={obj.profile__info_details_name}>{currentProfileInfo.fullName}</div>
-          <div>{currentProfileInfo.lookingForAJob ? `looking for a job` : `not looking`}</div>
-        </div>
-        <Status
-          status={props.status}
-          changeStatus={props.changeStatus}
-        ></Status>
-        <div className={obj.profile__info_details_container_row}>
-          <div>{currentProfileInfo.aboutMe}</div>
-          <div>{currentProfileInfo.lookingForAJobDescription}</div>
-        </div>
-        <div className={obj.profile__info_details_social}>social</div>
-      </div>
-
+      <ProfileDetails currentProfileInfo={currentProfileInfo} {...props}/>
     </div>
   );
 };
