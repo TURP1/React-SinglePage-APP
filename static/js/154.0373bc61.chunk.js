1c068b078b692f646ef656a1f4c69523c6aaee0f
TURP1 <turpicrypto@gmail.com>
2023-03-11 21:30:34 +0200

    Updates

--- static/js/154.0373bc61.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[154],{5154:function(n,u,e){e.r(u),e.d(u,{default:function(){return t}});e(2791);var r=e(184),t=function(){return(0,r.jsx)("div",{children:"MUSIC"})}}}]);
-//# sourceMappingURL=154.0373bc61.chunk.js.map
---
+++ static/js/154.d2fc1e63.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[154],{154:function(n,u,e){e.r(u),e.d(u,{default:function(){return t}});e(791);var r=e(184),t=function(){return(0,r.jsx)("div",{children:"MUSIC"})}}}]);
+//# sourceMappingURL=154.d2fc1e63.chunk.js.map
